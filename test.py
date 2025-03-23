fc6e9f8d09e4495e19af329c3c913b2e9a63a633
diwash <rimaldiwash7@gmail.com>
2025-03-23 07:17:57 +0545

    made a simple calculator

--- test.py
+++ test.py
@@ -1,5 +1,24 @@
-num=[6,9,89,3]
-for v in num:
-    print (v+1)
-    v=[6,7]
-    print(v)
+def sum(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def product(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+
+a=float(input("a:"))
+b=float(input("b:"))
+
+choose=input("choose:\n1=+\t2=-\n3=*\t4=/\n")
+
+if choose=="1":
+    print(sum(a,b))
+elif choose=="2":
+    print(sub(a,b))
+elif choose=="3":
+    print(product(a,b))
+elif choose=="4":
+    print(divide(a,b))
+else:
+    print("invalid")
